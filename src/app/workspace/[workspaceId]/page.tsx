cf2a2ac2e59358e63c865abc481b893eac071425
Zeying Zhou <zeyingzhou@Zeyings-MacBook-Air.local>
2025-01-06 11:57:37 -0500

    workspace member header

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,21 +1,7 @@
-"use client";
-import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
-import { useWorkspaceId } from "@/hooks/use-workspace-id";
-// interface WorkspaceIdPageProps {
-//     params: {
-//         workspaceId: string;
-//     };
-// };
-
-import { useParams } from "next/navigation";
-
 const WorkspaceIdPage = () => {
-    const workspaceId = useWorkspaceId();
-    const { data } = useGetWorkspace({id: workspaceId});
-
     return ( 
         <div>
-            Data: {JSON.stringify(data)}
+            Workspace id: page
         </div>
      );
 }
