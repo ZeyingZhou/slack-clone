5ec8cfeb1d6643063ba7861fc32c3824e8b9f9bd
Zeying Zhou <zeyingzhou@fa:bd:81:ca:73:20.home>
2025-01-17 18:23:20 -0500

    image upload

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -5,10 +5,11 @@ import { Delta, Op } from "quill/core";
 import "quill/dist/quill.snow.css";
 import { MutableRefObject, useEffect, useLayoutEffect, useRef, useState } from "react";
 import { Button } from "./ui/button";
-import { ImageIcon, Key, Smile } from "lucide-react";
+import { ImageIcon, Key, Smile, XIcon } from "lucide-react";
 import {Hint} from "./hint";
 import { cn } from "@/lib/utils";
 import { EmojiPopover } from "./emoji-popover";
+import Image from "next/image";
 
 type EditorValue = {
     image: File | null;
@@ -34,6 +35,7 @@ const Editor = ({
     variant = "create"
 } : EditorProps) => {
     const [text, setText] = useState(""); 
+    const [image, setImage] = useState<File | null>(null);
     const [isToolbarVisible, setIsToolbarVisible] = useState(true);
 
     const submitRef = useRef(onSubmit);
@@ -42,6 +44,7 @@ const Editor = ({
     const defaultValueRef = useRef(defaultValue);
     const containerRef = useRef<HTMLDivElement>(null);
     const disabledRef = useRef(disabled);
+    const imageElementRef = useRef<HTMLInputElement>(null);
 
     useLayoutEffect(() => {
         submitRef.current = onSubmit;
@@ -72,7 +75,14 @@ const Editor = ({
                         enter: {
                             key: "Enter",
                             handler: () => {
-                                //Todo submit form
+                                const text = quill.getText();
+                                const addedImage = imageElementRef.current?.files?.[0] || null;
+
+                                const isEmpty = !addedImage && text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
+                                if(isEmpty) return;
+                                
+                                const body = JSON.stringify(quill.getContents());
+                                submitRef.current?.({body, image: addedImage});
                             }
                         },
                         shift_enter: {
@@ -131,14 +141,45 @@ const Editor = ({
         quill?.insertText(quill?.getSelection()?.index || 0, emoji.native);
     }
 
-    const isEmpty = text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
-
-    console.log({isEmpty, text});
+    const isEmpty = !image && text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
 
     return ( 
         <div className="flex flex-col">
-            <div className="flex flex-col border border-slate-200 rounded-md overflow-hidden focus-within:border-slate-300 focus-within:shadow-sm transition bg-white">
+            <input
+                type="file"
+                accept="image/*"
+                ref={imageElementRef}
+                onChange={(event) => setImage(event.target.files![0])}
+                className="hidden"
+            />
+            <div className={cn(
+                "flex flex-col border border-slate-200 rounded-md overflow-hidden focus-within:border-slate-300 focus-within:shadow-sm transition bg-white",
+                disabled && "opacity-50"
+                )}>
                 <div ref={containerRef} className="h-full ql-custom"/>
+                {!!image && (
+                    <div className="p-2">
+                        <div className="relative size-[62px] flex items-center justify-center group/image">
+                            <Hint label="Remove Image">
+                                <button
+                                    onClick={() => {
+                                        setImage(null);
+                                        imageElementRef.current!.value = "";
+                                    }}
+                                    className="hidden group-hover/image:flex rounded-full bg-black/70 hover:bg-black absolute -top-2.5 -right-2.5 text-white size-6 z-[4] border-2 border-white items-center justify-center"
+                                >
+                                    <XIcon className="size-3.5 "/>
+                                </button>
+                            </Hint>
+                            <Image
+                                src={URL.createObjectURL(image)}
+                                alt="upload"
+                                fill
+                                className="rounded-xl overflow-hidden border object-cover"
+                            />
+                        </div>
+                    </div>
+                )}
                 <div className="flex px-2 pb-2 z-[5]">
                     <Hint label={isToolbarVisible ? "Hide formatting" : "Show formatting"}>
                         <Button 
@@ -165,7 +206,7 @@ const Editor = ({
                                 disabled={disabled}
                                 size="iconSm"
                                 variant="ghost"
-                                onClick={() => {}}
+                                onClick={() => imageElementRef.current?.click()}
                             >
                                 <ImageIcon className="size-4"/>
                             </Button>
@@ -176,14 +217,19 @@ const Editor = ({
                             <Button
                                 variant="outline"
                                 size="sm"
-                                onClick={() => {}}
+                                onClick={onCancel}
                                 disabled={disabled}
                             >
                                 Cancel
                             </Button>
                             <Button
                                 size="sm"
-                                onClick={() => {}}
+                                onClick={() => {
+                                    onSubmit({
+                                        body: JSON.stringify(quillRef.current?.getContents()),
+                                        image,
+                                    })
+                                }}
                                 disabled={disabled || isEmpty}
                                 className="bg-[#007a5a] hover:bg-[#007a5a]/80 text-white"
                             >
@@ -194,7 +240,12 @@ const Editor = ({
                     {variant === "create" && (
                         <Button 
                             disabled={disabled || isEmpty}
-                            onClick={() => {}}
+                            onClick={() => {
+                                onSubmit({
+                                    body: JSON.stringify(quillRef.current?.getContents()),
+                                    image,
+                                })
+                            }}
                             size="iconSm"
                             className={cn(
                                 "ml-auto",
