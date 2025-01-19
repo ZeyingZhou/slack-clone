b9ed0d1d27e6d053d486f9d4e3824ce8d44d2eb8
Zeying Zhou <zeyingzhou@Zeyings-MacBook-Air.local>
2025-01-19 16:37:21 -0500

    message list & channel hero

--- src/app/workspace/[workspaceId]/user-item.tsx
+++ src/app/workspace/[workspaceId]/user-item.tsx
@@ -47,9 +47,9 @@ export const UserItem = ({
             asChild
         >
             <Link href={`/workspace/${workspaceId}/member/${id}`}>
-                <Avatar className="size-5 rounded-md mr-1">
+                <Avatar className="size-5 mr-1">
                     <AvatarImage className="rounded-md" src={image}/>
-                    <AvatarFallback className="rounded-md bg-sky-500 text-white text-xs">
+                    <AvatarFallback>
                         {avatarFallback}
                     </AvatarFallback>
                 </Avatar>
